4eeea56c5b5e40359d7d33a51aea4f03e0593caa
Surayya-code <53233211+Surayya-code@users.noreply.github.com>
2019-09-26 21:17:06 +0400

    display

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-    $('.owl-carousel').owlCarousel();
+    $('.owl-carousel').owlCarousel({
+      items:3,
+      loop: true,
+      margin: 0,
+      autoplay: true,
+      nav: true,
+      dots: true,
+      smartSpeed: 1000,
+      navText: ['<span class="icon-arrow_back">', '<span class="icon-arrow_forward">']
+    });
   });
