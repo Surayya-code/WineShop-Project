ef7bd6c82a092e7216e7870ab92513f2973092b4
Surayya-code <53233211+Surayya-code@users.noreply.github.com>
2019-09-26 21:45:36 +0400

    INDEX

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
-      items:3,
+
+
+
+    $('#carousel-1').owlCarousel({
+      items:1,
       loop: true,
       margin: 0,
       autoplay: true,
@@ -9,4 +12,18 @@ $(document).ready(function(){
       smartSpeed: 1000,
       navText: ['<span class="icon-arrow_back">', '<span class="icon-arrow_forward">']
     });
+
+$('#carousel-2').owlCarousel({
+items:3,
+loop:true,
+margin:0,
+autoplay: true,
+nav: true,
+dots: true,
+smartSpeed: 1000,
+navText: ['<span class="icon-arrow_back">', '<span class="icon-arrow_forward">']
+});
+
+
+
   });
